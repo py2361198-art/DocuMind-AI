a655c2296574e6e057204e21e771498dbd2e231f
Prashant Yadav <py2361198@gmail.com>
2026-08-20 23:19:28 +0530

    Create chunking.py

---
+++ backend/chunking.py
@@ -0,0 +1,15 @@
+def split_text(text, chunk_size=1000, overlap=200):
+    chunks = []
+
+    start = 0
+
+    while start < len(text):
+        end = start + chunk_size
+        chunk = text[start:end]
+
+        if chunk.strip():
+            chunks.append(chunk.strip())
+
+        start += chunk_size - overlap
+
+    return chunks
